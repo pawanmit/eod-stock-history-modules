f938b0db872b736e26af98bb90bfd7aafec0532d
M820787 <pawan.mittal@macys.com>
2016-11-22 12:00:02 -0800

    fixing shell script

--- refresh_tickers_and_history.sh
+++ refresh_tickers_and_history.sh
@@ -2,4 +2,4 @@
 python data-collection/db/ddl/drop-schema.py
 python data-collection/db/ddl/create-schema.py
 python data-collection/fetch_tickers.py
-python data-collection/fetch_tickers.py
+python data-collection/fetch_tickers_history.py
